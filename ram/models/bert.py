e9e9e1d9ac4fe5a9a16c64c3c7025fbaa0070b74
tqth <tqthinh2611@gmail.com>
2026-03-26 14:03:40 +0700

    Update bert.py

--- ram/models/bert.py
+++ ram/models/bert.py
@@ -37,16 +37,11 @@ from transformers.modeling_outputs import (
     TokenClassifierOutput,
 )
 from transformers.modeling_utils import (
-    PreTrainedModel
-    # find_pruneable_heads_and_indices,
-    # prune_linear_layer,
-)
-from transformers.pytorch_utils import (
+    PreTrainedModel,
     apply_chunking_to_forward,
     find_pruneable_heads_and_indices,
-    prune_linear_layer
+    prune_linear_layer,
 )
-# from transformers.pytorch_utils import apply_chunking_to_forward
 from transformers.utils import logging
 from transformers.models.bert.configuration_bert import BertConfig
 
