460f2c8613c7de4000555f7eb7437be1073c0fc4
tqth <tqthinh2611@gmail.com>
2026-03-26 13:50:09 +0700

    Update bert.py

--- ram/models/bert.py
+++ ram/models/bert.py
@@ -38,10 +38,10 @@ from transformers.modeling_outputs import (
 )
 from transformers.modeling_utils import (
     PreTrainedModel,
-    apply_chunking_to_forward,
     find_pruneable_heads_and_indices,
     prune_linear_layer,
 )
+from transformers.pytorch_utils import apply_chunking_to_forward
 from transformers.utils import logging
 from transformers.models.bert.configuration_bert import BertConfig
 
